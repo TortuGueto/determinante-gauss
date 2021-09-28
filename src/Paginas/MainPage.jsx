dc71380b1759bfa9cacedb197f22b69514fc3503
TortuGueto <TortuGueto@users.noreply.github.com>
2021-09-28 14:11:16 -0300

    opa eae teste

--- src/Paginas/MainPage.jsx
+++ src/Paginas/MainPage.jsx
@@ -3,7 +3,7 @@ function MainPage() {
       <div className="flex justify-center ">
          <div className="flex flex-row justify-center w-10/12 shadow-md p-5 px-7 rounded-3xl m-2">
             <p className="bg-white ring-1 w-8/12 rounded-xl ring-gray-200 p-2 m-1">
-               Esse site aqui tem como objetivo realizar automaticamente as operações de 
+               Esse site tem como objetivo realizar automaticamente as operações de 
             </p>
 
             <p className="bg-white ring-1 w-4/12 rounded-xl ring-gray-200 p-2 m-1">
